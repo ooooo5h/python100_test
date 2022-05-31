ed62ab1a9d59ca5990c21d448d6b85c1220c43b1
Jeon, Eun Hyung <78359232+ooooo5h@users.noreply.github.com>
2022-05-31 22:27:22 +0900

    #1

---
+++ python.py
@@ -0,0 +1,12 @@
+# 1. 리스트의 삭제
+# 내가 푼 풀이
+nums = [100, 200, 300, 400, 500]
+nums.remove(400)
+nums.remove(500)
+print(nums)
+
+# 답안 풀이
+nums1 = [100, 200, 300, 400, 500]
+nums1.pop()
+nums1.pop()
+print(nums1)
